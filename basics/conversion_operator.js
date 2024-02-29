693622d577d9edf6e61fa2b73184261b7c6097d4
Nit89 <nitesh382singh.com>
2024-02-29 22:07:34 +0530

    first commit

---
+++ basics/conversion_operator.js
@@ -0,0 +1,18 @@
+let score = 32;
+// console.log(typeof score);
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1 , false => 0
+
+// 1 =>  true ;  0 => false
+// "" =>false
+//  "hitesh" => true
+
+console.log("1" + 2);
+console.log(1 + 2 + "3");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+console.log(num1, num2, num3);
